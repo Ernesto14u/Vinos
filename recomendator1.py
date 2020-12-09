7870a93091ebb86b2f23d242fbb57982f8ca2467
Ernesto del Puerto <71317852+Ernesto14u@users.noreply.github.com>
2020-12-09 16:26:15 +0100

    Recomendator

---
+++ recomendator1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 16:13:06 2020
+
+@author: Ernesto
+"""
+import sklearn
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+vinotfidf = pd.read_csv('vino_var_sab.csv')
+
+st.title('Wine Recomendator')
+
+st.write(vinotfidf)
+
