cd3605aefc13678583c9d803c0cb23ef51278329
Ernesto del Puerto <71317852+Ernesto14u@users.noreply.github.com>
2020-12-10 02:45:44 +0100

    Refresh

--- recomendator1.py
+++ recomendator1.py
@@ -20,6 +20,47 @@ color=st.selectbox("Qué color de vino prefieres?", ('Red','Rose','White', 'It´
 if color !='It´s the same':
     vinotfidf= vinotfidf.loc[vinotfidf['Color']== color]
 respuestas = list()
+
+
+progress_bar = st.progress(0)
+status_text = st.empty()
+chart = st.line_chart(np.random.randn(10, 2))
+
+for i in range(100):
+    # Update progress bar.
+    progress_bar.progress(i + 1)
+
+    new_rows = np.random.randn(10, 2)
+
+    # Update status text.
+    status_text.text(
+        'The latest random number is: %s' % new_rows[-1, 1])
+
+    # Append data to the chart.
+    chart.add_rows(new_rows)
+
+    # Pretend we're doing some computation that takes time.
+    time.sleep(0.1)
+
+status_text.text('Done!')
+st.balloons()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 st.subheader('Crea tu vino')
 respuestas.append(st.slider('apple:', 1, 5, 1))
 respuestas.append(st.slider('character:', 1, 5, 1))
