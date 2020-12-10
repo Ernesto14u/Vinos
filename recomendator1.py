fd5f22057319b80712d73305397256d2471e2586
Ernesto del Puerto <71317852+Ernesto14u@users.noreply.github.com>
2020-12-10 02:15:02 +0100

    Refresh

--- recomendator1.py
+++ recomendator1.py
@@ -13,7 +13,7 @@ import seaborn as sns
 import streamlit as st
 
 
-vinotfidf = pd.read_excel("vino_var_sab.xlsx")
+vinotfidf = pd.read_('dfcito.csv')
 
 st.title('Wine Recommender')
 color=st.selectbox("Qué color de vino prefieres?", ('Red','Rose','White', 'It´s the same'))
